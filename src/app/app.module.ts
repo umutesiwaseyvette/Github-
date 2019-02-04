096c07f772a894ffa636c4506da942992e993f60
umutesiwase yvette <rwjpyy@gmail.com>
2019-02-04 13:54:44 +0200

    add a folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { ImagesComponent } from './images/images.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    ImagesComponent
   ],
   imports: [
     BrowserModule,
