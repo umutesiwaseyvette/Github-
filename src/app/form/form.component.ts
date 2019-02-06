266a560224a329c0560c74e7c60f3840e0ba9550
umutesiwase yvette <rwjpyy@gmail.com>
2019-02-06 11:17:47 +0200

    make change

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
 import {HttpClient} from '@angular/common/http'
-import { environment } from 'src/environments/environment';
 import { Repository } from '../repository';
 import {ServiceService} from'../service.service';
 
@@ -13,36 +12,38 @@ import {ServiceService} from'../service.service';
   providers:[ServiceService]
 })
 export class formComponent implements OnInit {
+  user: User;
   submitSearch(name) {
-    this.ServiceService .getProfileInfo(name.target.value);
-    this. Repository.getRepoInfo(name.target.value);
+    this.service .getProfileInfo(name.target.value);
+    this. service.getRepoInfo(name.target.value);
   }
 
   username= new User("",'');
-  repos=new Repository("",0,"");
-  constructor(private http:HttpClient) { 
-    this.repos=new Repository("",0,"");
+  public_repos=new Repository("",0,"");
+  constructor(private http:HttpClient, private service:ServiceService) { 
+    this.user = this.service.user;
+    this.public_repos=new Repository("",0,"");
     }
   Check(){
     interface ApiResponse{
       name:string;
       public_repos:number;
-      created_at:Date;
       followers:string;
     }
-    this.http.get<ApiResponse>("https://api.github.com/users/"+this.username+"?access_token=" +environment.api_key)
+    this.http.get<ApiResponse>("https://api.github.com/users/"+this.username+"?access_token=b9fc7ad7493527d9ae03754c2d2728bc8f705c55")
     .subscribe((data:any)=>{
       
       // console.log(data);
-      this.repos.name= data.name;
-      this.repos.public_repos=data.public_repos;
-      this.repos.followers=data.followers;
+      this.public_repos.name= data.name;
+      this.public_repos.public_repos=data.public_repos;
+      this.public_repos.followers=data.followers;
     });
   }
   // console.log(this.username)
 
 
   ngOnInit() {
+    this.user = this.service.user;
   }
 
 }
